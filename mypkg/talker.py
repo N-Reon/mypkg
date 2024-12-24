8c62613902aabc3230c6408b3a27bea5a0d9b1d9
N.Reon <s23C1102EK@s.chibakoudai.jp>
2024-12-24 15:34:37 +0900

    add a command

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,21 +1,24 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int16
+from std_msgs.msg import Float16
 
-rclpy.init()
-node = Node("talker")
-pub = node.create_publisher(Int16, "countup", 10)
-n = 0
+class Talker(Node):
+    def __init__(salf):
+        super().__init__("talker")
+        self.pub = self.create_publisher(Float16, "countup", 10)
+        self.create_timer(1.0, self.cd)
+        self.n = 0.0
+        self.m = 0.0
 
 
-def cb():
-    global n
-    msg = Int16()
-    msg.data = n
-    pub.publish(msg)
-    n += 1
-
+    def cb(self):
+        global n
+        msg = Float16()
+        msg.data = self.n
+        self.pub.publish(msg)
+        self.m += 1.0
+        self.n = 100000.0 * 0.18 / 365.0 * self.m
 
 def main():
-    node.create_timer(0.5, cb)
+    node.create_timer(1.0, cb)
     rclpy.spin(node)
